6bd5c2031acc9ab78f620425232ff3fd0d8e4190
Iliasmedia <21534@ma-web.nl>
2017-05-10 18:49:59 +0200

    Update footer.php

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,4 +1,4 @@
    <footer class="footer-bottom">
         2017 &copy; <?php echo SITE_NAME; ?> <i class="em em-v"></i> is medemogelijk gemaakt door Ilias & Wesley van MD1A <i class="em em-heart_eyes"></i><br>
-		PHP 7 - PDO - JQUERY - MVC STRUCTUUR
+		PHP 7 (AJAX) - PDO - JQUERY - MVC
     </footer>
